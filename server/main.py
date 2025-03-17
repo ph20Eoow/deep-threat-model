30f1d10e6bd5a3ab52fdc318bec707f3f237505a
guru <tonypakhong@gmail.com>
2025-03-16 23:40:21 -0700

    remove: logfire

--- server/main.py
+++ server/main.py
@@ -1,40 +1,16 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
 from api import tm
-import logfire
-from config import config
-if config.LOGFIRE_API_KEY:
-    logfire.configure(token=config.LOGFIRE_API_KEY)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup: Initialize admin user
-    try:
-        # driver = Neo4jDriver()
-        # user_repo = UserRepository(driver)
-        # # Hash the default admin password
-        # hashed_password = get_password_hash(db_settings.default_admin_password)  # Default password, should change after first login
-        # admin_user = await user_repo.initialize_admin(hashed_password)
-        # if admin_user:
-            print("✅ Admin user initialized successfully")
-        # else:
-        #     print("ℹ️ Admin user already exists")
-    except Exception as e:
-        print(f"❌ Error initializing admin user: {str(e)}")
-    
-    yield  # Server is running
-    # Shutdown: Clean up resources if needed
 
 app = FastAPI(
-    title="API for /rTM",
-    description="API for /rTM",
+    title="API for deep-tm",
+    description="API for deep-tm",
     version="1.0.0",
-    lifespan=lifespan
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # frontend origin
+    allow_origins=["http://localhost:5173"],  # frontend 
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
@@ -43,7 +19,7 @@ app.include_router(tm.router, prefix="/api")
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to your FastAPI application!"}
+    return {"message": "Welcome to deep-tm!"}
 
 
 if __name__ == "__main__":
